7f280c9b44583647d3fdb36a43b98508c7500c7a
markandeyshuklae <btw.its.markandey@gmail.com>
2025-05-19 10:13:12 +0530

    header added on detail page

--- src/pages/Copondetail/index.js
+++ src/pages/Copondetail/index.js
@@ -1,8 +1,13 @@
 import CouponDetail from "../../component/coupondetail/Coupondetail";
+import Heador from "../../component/landing/Heador";
+import Footer from "../../component/homepage/Footer";
+
 function copondetail(){
     return(
         <>
+        <Heador/>
          <CouponDetail/>
+         <Footer/>
         </>
     )
 }
